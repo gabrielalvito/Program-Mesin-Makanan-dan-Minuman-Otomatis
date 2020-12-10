7c5f72af1fa27b9bf2456a8e2dab2d2d20aecb1b
gabrielalvito <73921676+gabrielalvito@users.noreply.github.com>
2020-12-10 19:32:11 +0700

    Update Program Mesin Makanan dan Minuman.py
    Menambahkan menu utama program

--- Program Mesin Makanan dan Minuman.py
+++ Program Mesin Makanan dan Minuman.py
@@ -1,1 +1,18 @@
-#PROGRAM MESIN MAKANAN DAN MINUMAN
+# PROGRAM MESIN MAKANAN DAN MINUMAN
+# Menu Utama
+def menampilkanjenis():
+  print("==================================================")
+  print("Program Mesin Penjual Makanan dan Minuman Otomatis")
+  print("==================================================")
+  print("1. Makanan Ringan")
+  print("2. Minuman Dingin")
+  print("0. Keluar")
+  
+  pilih = str(input("Masukkan Pilihan : "))
+  
+  if(pilih == "1"):
+    makananringan()
+  elif(pilih == "2"):
+    minumandingin()
+  elif(pilih =="0"):
+    Membeli_lagi()
