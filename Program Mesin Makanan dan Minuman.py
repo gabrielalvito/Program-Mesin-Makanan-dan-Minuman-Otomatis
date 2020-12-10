b2c7cfb13db29fb0b6f402f44eed2003a8046f0e
gabrielalvito <73921676+gabrielalvito@users.noreply.github.com>
2020-12-10 22:00:34 +0700

    Update Program Mesin Makanan dan Minuman.py

--- Program Mesin Makanan dan Minuman.py
+++ Program Mesin Makanan dan Minuman.py
@@ -1,64 +1,64 @@
 # PROGRAM MESIN MAKANAN DAN MINUMAN
 def menampilkanjenis():
-  print("==================================================")
-  print("Program Mesin Penjual Makanan dan Minuman Otomatis")
-  print("==================================================")
-  print("1. Makanan Ringan")
-  print("2. Minuman Dingin")
-  print("0. Keluar")
+    print("==================================================")
+    print("Program Mesin Penjual Makanan dan Minuman Otomatis")
+    print("==================================================")
+    print("1. Makanan Ringan")
+    print("2. Minuman Dingin")
+    print("0. Keluar")
   
-  pilih = str(input("Masukkan Pilihan : "))
+    pilih = str(input("Masukkan Pilihan : "))
   
-  if(pilih == "1"):
-    makananringan()
-  elif(pilih == "2"):
-    minumandingin()
-  elif(pilih =="0"):
-    Membeli_lagi()
+    if(pilih == "1"):
+      makananringan()
+    elif(pilih == "2"):
+     minumandingin()
+    elif(pilih =="0"):
+      Membeli_lagi()
 
 #MAKANAN RINGAN
 def makananringan():
-mr = ["1. Chitato", "2. Maitos", "3. Qtela", "4. Taro", "5. Jetz", "6.Pilus", "7. SilverQueen", "8. Cadbury"]
-  print("Pilih Makanan")
-  print(mr[0])
-  print(mr[1])
-  print(mr[2])
-  print(mr[3])
-  print(mr[4])
-  print(mr[5])
-  print(mr[6])
-  print(mr[7])
-  print("0. Keluar")
-  print("===========================")
+    mr = ["1. Chitato", "2. Maitos", "3. Qtela", "4. Taro", "5. Jetz", "6.Pilus", "7. SilverQueen", "8. Cadbury"]
+    print("Pilih Makanan")
+    print(mr[0])
+    print(mr[1])
+    print(mr[2])
+    print(mr[3])
+    print(mr[4])
+    print(mr[5])
+    print(mr[6])
+    print(mr[7])
+    print("0. Keluar")
+    print("===========================")
     
-  pilih = str(input("Masukkan Pilihan     : "))
+    pilih = str(input("Masukkan Pilihan     : "))
       
 
-  if (pilih == "1"):
-    Chitato()
-  elif (pilih == "2"):
-    Maitos()
-  elif (pilih == "3"):
-    Qtela()
-  elif (pilih == "4"):
-    Taro()
-  elif (pilih == "5"):
-    Jetz()
-  elif (pilih == "6"):
-    Pilus()
-  elif (pilih == "7"):
-    SilverQueen()
-   elif (pilih == "8"):
-    Cadbury()
-  elif (pilih == "0"): 
-    Membeli_lagi()
-  else:
-    print("Makanan Tidak Tersedia")
-    Kembali_ke_awal() 
+    if (pilih == "1"):
+        Chitato()
+    elif (pilih == "2"):
+        Maitos()
+    elif (pilih == "3"):
+        Qtela()
+    elif (pilih == "4"):
+        Taro()
+    elif (pilih == "5"):
+        Jetz()
+    elif (pilih == "6"):
+        Pilus()
+    elif (pilih == "7"):
+        SilverQueen()
+    elif (pilih == "8"):
+        Cadbury()
+    elif (pilih == "0"): 
+        Membeli_lagi()
+    else:
+        print("Makanan Tidak Tersedia")
+        Kembali_ke_awal() 
     
 #MINUMAN DINGIN
 def minumandingin():
-md = ["1. Aqua Botol","2. Teh Pucuk","3. Pocari Sweat", "4. Freshtea", "5. Good Day", "6. Nestcafe"]
+    md = ["1. Aqua Botol","2. Teh Pucuk","3. Pocari Sweat", "4. Freshtea", "5. Good Day", "6. Nestcafe"]
     print("Pilih Minuman")
     print(md[0])
     print(md[1])
@@ -99,7 +99,7 @@ def Membeli_lagi():
         input("Tekan Enter untuk keluar")
         exit
         
-# Kembali ke awal
+# KEMBALI KE AWAL
 def Kembali_ke_awal():
     input("Tekan Enter untuk kembali...")
     print("")
@@ -166,7 +166,7 @@ def Jetz():
         print("Uang Anda Tidak Mencukupi")
         Kembali_ke_awal()
 
- def Pilus():
+def Pilus():
     HargaP = 4000
     x = int(input("Masukkan Uang Anda   : "))
     if (x >= HargaP):
